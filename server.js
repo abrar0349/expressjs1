16a40ce3506c5b22b78d6fb17cea9b715e3e0d68
abrar0349 <abrar0349khan@gmail.com>
2025-01-03 17:34:40 +0500

    expressjs file server code

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = require('./routers/')
+
+
+
+const app = express()
+
+app.use('/api',router)
+
+
+
+
+app.listen(3000, () => console.log('Server ready'))
+
